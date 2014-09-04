84c7d3eaa8af7081e1dc809ff6754ae89b35b002
libovness <jonathan.libov@gmail.com>
2014-09-04 10:01:49 -0400

    Broken tweet import script

--- cloud/main.js
+++ cloud/main.js
@@ -1,3 +1,4 @@
+
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
   
@@ -7,36 +8,43 @@ var oauth = require('cloud/oauth.js');
 var sha = require('cloud/sha1.js');
   
   
-function getTeam(placeShort) {
+function getTeamByPlaceShort(placeShort) {
     var team = Parse.Object.extend("Team");
     var query = new Parse.Query(team);
     query.equalTo("placeShort",placeShort);
-    return query.first();
+    team.resolve(query.first());
 } 
- 
-function getLastTweet(team) {
-    var lastTwitterPoll = Parse.Object.extend("LastTwitterPoll");
-    var lastTwitterPollQuery = new Parse.Query(lastTwitterPoll);
-    lastTwitterPollQuery.equalTo("team",team);    
-    lastTwitterPollQuery.first().then(function(lastTweet) {
-    if (!lastTweet) {
-            //
-        } else {
-            return lastTweet.get('lastTweetId');
-        }
-    }).then(function(error) {
-        return error;
-    });
+
+var theCurrTeam;
+var maxTweetId;
+
+function getCurrTeam() {
+    var lastTwitterPollQuery = new Parse.Query("LastTwitterPoll");
+    lastTwitterPollQuery.ascending('updatedAt');
+    lastTwitterPollQuery.first().then(function(result) {
+        console.log(JSON.stringify(result));
+        maxTweetId = result.tweetId;
+        var currTeam = result.get('team');
+        var objectId = JSON.stringify(currTeam).substring(13,23);
+        var teamQuery = new Parse.Query("Team");
+        theCurrTeam = teamQuery.get(objectId);
+        console.log(theCurrTeam);
+        return theCurrTeam;
+    }), function() {
+        console.log(error);
+    };
 };
-  
-function getTweets(team) {
-    var maxTweetId = getLastTweet(team);
+
+function getTweets(currTeam) {   
+    var currTeam = theCurrTeam;
+    console.log('the currteam is ' + JSON.stringify(currTeam));
+
     if (!maxTweetId) {
-        var urlLink = 'https://api.twitter.com/1.1/lists/statuses.json?slug=' + team.get('nickname') + '&owner_screen_name=FiftyApp';
+        var urlLink = 'https://api.twitter.com/1.1/lists/statuses.json?slug=' + currTeam.nickname + '&owner_screen_name=FiftyApp';
     } else {
-        var urlLink = 'https://api.twitter.com/1.1/lists/statuses.json?slug=' + team.get('nickname')  + '&owner_screen_name=FiftyApp&since_id=' + maxTweetId;
+        var urlLink = 'https://api.twitter.com/1.1/lists/statuses.json?slug=' + currTeam.nickname + '&owner_screen_name=FiftyApp&since_id=' + maxTweetId;
     }
-    console.log('the url link is ' + urlLink)
+
     var consumerSecret = "ne0zanSI5IiKg9DNFKvqRO0F7BJfvjMUEWV05qOJU2scYUsbCc";
     var tokenSecret = "PJGcgRga4bbeeZ6dfoUO1q5IhrgsnQJvyBu5gcFUzxOLR";
     var oauth_consumer_key = "RhvIw0lN08aTXdzfHxSTnmMW5";
@@ -75,52 +83,56 @@ function getTweets(team) {
     var sig = oauth.getParameter(message.parameters, "oauth_signature") + "=";
     /* console.log("Non-Encode Signature: " + sig);  */
     var encodedSig = oauth.percentEncode(sig); //finally you got oauth signature
-    /* console.log("Encoded Signature: " + encodedSig);  */
- 
-    Parse.Cloud.httpRequest({
+    /* console.log("Encoded Signature: " + encodedSig); */
+
+    return Parse.Cloud.httpRequest({
         method: 'GET',
         url: urlLink,
         headers: {
             "Authorization": 'OAuth oauth_consumer_key="RhvIw0lN08aTXdzfHxSTnmMW5", oauth_nonce="' + nonce + '", oauth_signature="' + encodedSig + '", oauth_signature_method="HMAC-SHA1", oauth_timestamp="' + timestamp + '",oauth_token="17611064-ndOOXimcEyq9RtylywbTqTeAs1jc4pUnf3cP0QPzM", oauth_version="1.0"'
         },
         body: {
-        }).then(function(httpResponse) {
-            console.log("FML");
-            console.log("response is " + httpResponse.text);
-            return httpResponse.text;
-        }, function(error) {
-            return error;
-        };
-    }); 
-  
-function saveTweets(tweets) {
-    var TweetClass = Parse.Object.extend("Tweet");
-    var theTweetData = tweets;
-    var mostRecentTweetId = theTweetData[0].id_str;
-    var tweetArray = [];
- 
-    var lastTwitterPoll = Parse.Object.extend("LastTwitterPoll");
-    var lastTwitterPollQuery = new Parse.Query(lastTwitterPoll);
-    lastTwitterPollQuery.equalTo(team);
- 
-    lastTwitterPollQuery.first().then(function(lastPoll) {
-        if(!lastPoll) {
-            var twitterPoll = new lastTwitterPoll();
-            twitterPoll.set("lastTweetId",mostRecentTweetId);
-            twitterPoll.set("team",team);
-            twitterPoll.save();
-        } else {
-            results.set("lastTweetId",mostRecentTweetId);
-            results.save();
-        }}).then(function(error) {
-            return error;
+        }, success: function(httpResponse) {
+            console.log('tweets pulled for ' + currTeam.get('nickname'));
+        }, error: function(httpResponse) {
+            console.log('tweets failed to pull!');
+        }
     });
- 
-     
-    for (i = 0; i < theTweetData.length; i++) {
+
+};
+
+
+function saveTweets(tweets) {
+    console.log('hello ' + currTeam.get('nickname'));
+    var TweetClass = Parse.Object.extend("Tweet");
+    var firstParsedTweet = tweets[0];
+    var mostRecentTweetId = firstParsedTweet.id_str;
+    var tweetArray = [];
+    var lastTwitterPollQuery = new Parse.Query("LastTwitterPoll");
+    lastTwitterPollQuery.equalTo("team", currTeam);
+    lastTwitterPollQuery.first({
+        success: function(poll) {
+            if(!poll) {
+                var newTwitterPoll = Parse.Object.extend("LastTwitterPoll");
+                var newTWitterPollObj = new newTwitterPoll();
+                newTWitterPollObj.set('lastTweetId',mostRecentTweetId);
+                newTWitterPollObj.set('team',currTeam);
+                newTWitterPollObj.save();
+            } else {
+                console.log('poll is ' + JSON.stringify(poll));
+                poll.set("lastTweetId", mostRecentTweetId);
+                poll.save();
+            }
+        },
+        error: function(poll) {
+            //
+        }
+    });
+    
+    
+    for (i = 0; i < tweets.length; i++) {
         var tweetSet = new TweetClass();
-        var tweet = theTweetData[i];
- 
+        var tweet = tweets[i];
         if (tweet['entities']['media'] != null) {
             var media = tweet['entities']['media'];
             var media_url = media[0]['media_url']
@@ -128,7 +140,7 @@ function saveTweets(tweets) {
         };
         tweetSet.set("text",tweet.text);
         tweetSet.set("tweetId",tweet.id_str);
-        tweetSet.set("team",team);
+        tweetSet.set("team",currTeam);
         tweetSet.set("userImageURL",tweet['user']['profile_image_url']);
         tweetSet.set("userId",tweet['user']['id']);    
         tweetSet.set("userName",tweet['user']['name']);
@@ -136,26 +148,34 @@ function saveTweets(tweets) {
         tweetSet.set("tweetCreatedAt",cleanedDate);                    
         tweetArray.push(tweetSet);
     }
-  
-    Parse.Object.saveAll(tweetArray, {
+
+    return Parse.Object.saveAll(tweetArray, {
         success: function (results) {
-            response.success(results);
+            results;
         },
         error: function (error) {
-            response.error(error);
+            error;
         }
     });
 };
-  
-var currTeam;
- 
+   
+
 Parse.Cloud.job("updateAllTwitters", function(request, status) {
-  var team = Parse.Object.extend("Team");
-  var query = new Parse.Query(team);
-  query.first(function(theTeam) {
-    return getTweets(theTeam);
-    console.log(getTweets(theTeam));
-  }/*).then(function(tweets) {
-    saveTweets(tweets);
-  }*/);
+    getCurrTeam();
+    /*.then(function(result) {
+        return result;
+    }).then(function(result) {
+        return getTweets();
+    }).then(function(tweets) {
+        var tweetText = tweets.text;
+        var parsedTweets = JSON.parse(tweetText);
+        return saveTweets(parsedTweets);
+    }).then(function() {
+        status.success("All good!");
+    }, function(error) {
+        status.error("No good!")
+    }); */
 });
+
+    
+
